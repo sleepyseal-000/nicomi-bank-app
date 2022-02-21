51f89ff46baa47f59d20dbe0f132f738307b490d
sleepyseal-000 <56409287+sleepyseal-000@users.noreply.github.com>
2022-02-21 15:14:21 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template,request, flash
+from website import create_app
 
-app =  Flask(__name__)
+app = create_app()
 
-@app.route("/")
-def main():
-    return render_template("index.html")
+if __name__== '__main__':
+    app.run(debug=True)
+
